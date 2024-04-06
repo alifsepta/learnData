78a9e892443d38fa82159f19eaa1e8d1b3257e42
alifsepta <alif.techdev@gmail.com>
2024-04-06 09:16:40 +0700

    Specifying module imports

---
+++ Write Modul in Python/main2.py
@@ -0,0 +1,8 @@
+# specified import module
+
+from hello import mencari_luas_persegi_panjang, nama
+
+persegi_panjang_pertama = mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang_pertama)
+
+print(nama)
