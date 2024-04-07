dda7d6ccbf5beaffc2f9970f791f2ce3cfedf05b
alifsepta <alif.techdev@gmail.com>
2024-04-07 09:00:38 +0700

    style guide checking

---
+++ Style Guide Python/kalkulator.py
@@ -0,0 +1,9 @@
+#based on PEP8 this code is incorrect
+#try to test using pycodestyle, pylint, flake8
+class Kalkulator:
+    """kalkulator tambah kurang"""
+    def __init__(self, _i):
+        self.i = _i
+    def tambah(self, _i): return self.i + _i
+    def kurang(self, _i):
+    return self.i - _i
