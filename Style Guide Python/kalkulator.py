d9d4eb1102bdb9b6e80d75bac5edb6ff6a26d2a6
alifsepta <alif.techdev@gmail.com>
2024-04-07 09:11:29 +0700

    reformat code

--- Style Guide Python/kalkulator.py
+++ Style Guide Python/kalkulator.py
@@ -1,9 +1,26 @@
-#based on PEP8 this code is incorrect
-#try to test using pycodestyle, pylint, flake8
+# based on PEP8 this code is incorrect
+# try to test using pycodestyle, pylint, flake8
+
+# class Kalkulator:
+#     """kalkulator tambah kurang"""
+#     def __init__(self, _i):
+#         self.i = _i
+#     def tambah(self, _i): return self.i + _i
+#     def kurang(self, _i):
+#     return self.i - _i
+
+
+# reformat this code using black, yapf, autopep8
+
+
 class Kalkulator:
     """kalkulator tambah kurang"""
+
     def __init__(self, _i):
         self.i = _i
-    def tambah(self, _i): return self.i + _i
+
+    def tambah(self, _i):
+        return self.i + _i
+
     def kurang(self, _i):
-    return self.i - _i
+        return self.i - _i
