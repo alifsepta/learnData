717099ea44f5d52387ae65e826b486eac152a5c6
alifsepta <alif.techdev@gmail.com>
2024-04-08 10:11:38 +0700

    try library parser

---
+++ Library/libraryParser.py
@@ -0,0 +1,16 @@
+# import argparse
+ 
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-o', '--output', action='store_true', help="tampilkan output")
+# args = parser.parse_args()
+ 
+# if args.output:
+#    print("Halo, ini merupakan sebuah output dari libraryParser.py")
+
+import argparse
+ 
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--nama', required=True, help="Masukkan Nama Anda")
+args = parser.parse_args()
+ 
+print("Terima kasih telah menggunakan panggildicoding.py, "+args.nama)
